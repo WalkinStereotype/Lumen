937ce8049ac3d67be1b863a0bf009742dea2a241
Dre Suguitan <ddre2026@gmail.com>
2025-03-21 21:26:45 -0700

    Sticky enemies have correct orientation when traversing their platform

--- Assets/Scripts/Enemy/EnemyStickyController.cs
+++ Assets/Scripts/Enemy/EnemyStickyController.cs
@@ -7,6 +7,14 @@ public class EnemyStickyController : MonoBehaviour
     public int currentWaypointIndex = 0;
     public SpriteRenderer sr;
 
+    private Vector2 lastPosition;
+
+    void Start()
+    {
+        sr = GetComponent<SpriteRenderer>();
+        lastPosition = transform.position;
+    }
+
     void Update()
     {
         if (waypoints.Length == 0) return;
@@ -14,14 +22,14 @@ public class EnemyStickyController : MonoBehaviour
 
         Transform targetWaypoint = waypoints[currentWaypointIndex];
         
-        if (targetWaypoint.position.x < transform.position.x)
-        {
-            sr.flipX = true; // Moving left
-        }
-        else if (targetWaypoint.position.x > transform.position.x)
-        {
-            sr.flipX = false; // Moving right
-        }
+        // if (targetWaypoint.position.x < transform.position.x)
+        // {
+        //     sr.flipX = true; // Moving left
+        // }
+        // else if (targetWaypoint.position.x > transform.position.x)
+        // {
+        //     sr.flipX = false; // Moving right
+        // }
 
         transform.position = Vector2.MoveTowards(transform.position, targetWaypoint.position, speed * Time.deltaTime);
 
@@ -29,5 +37,16 @@ public class EnemyStickyController : MonoBehaviour
         {
             currentWaypointIndex = (currentWaypointIndex + 1) % waypoints.Length;
         }
+
+        //Logic for sprite orientation
+        Vector2 movementDirection = (Vector2)transform.position - lastPosition;
+        if (movementDirection.magnitude > 0.01f) // Avoid jittering
+        {
+            float angle = Mathf.Atan2(movementDirection.y, movementDirection.x) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.Euler(0, 0, angle);
+        }
+
+        lastPosition = transform.position; // Update last position
+
     }
 }
