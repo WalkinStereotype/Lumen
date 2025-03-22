c9959763f464c514f7dd0c8e2fb3a0d0542e175b
Dre Suguitan <ddre2026@gmail.com>
2025-03-21 21:31:23 -0700

    Reverted enemy orientation changes

--- Assets/Scripts/Enemy/EnemyStickyController.cs
+++ Assets/Scripts/Enemy/EnemyStickyController.cs
@@ -7,12 +7,9 @@ public class EnemyStickyController : MonoBehaviour
     public int currentWaypointIndex = 0;
     public SpriteRenderer sr;
 
-    private Vector2 lastPosition;
-
     void Start()
     {
         sr = GetComponent<SpriteRenderer>();
-        lastPosition = transform.position;
     }
 
     void Update()
@@ -22,14 +19,14 @@ public class EnemyStickyController : MonoBehaviour
 
         Transform targetWaypoint = waypoints[currentWaypointIndex];
         
-        // if (targetWaypoint.position.x < transform.position.x)
-        // {
-        //     sr.flipX = true; // Moving left
-        // }
-        // else if (targetWaypoint.position.x > transform.position.x)
-        // {
-        //     sr.flipX = false; // Moving right
-        // }
+        if (targetWaypoint.position.x < transform.position.x)
+        {
+            sr.flipX = true; // Moving left
+        }
+        else if (targetWaypoint.position.x > transform.position.x)
+        {
+            sr.flipX = false; // Moving right
+        }
 
         transform.position = Vector2.MoveTowards(transform.position, targetWaypoint.position, speed * Time.deltaTime);
 
@@ -37,16 +34,6 @@ public class EnemyStickyController : MonoBehaviour
         {
             currentWaypointIndex = (currentWaypointIndex + 1) % waypoints.Length;
         }
-
-        //Logic for sprite orientation
-        Vector2 movementDirection = (Vector2)transform.position - lastPosition;
-        if (movementDirection.magnitude > 0.01f) // Avoid jittering
-        {
-            float angle = Mathf.Atan2(movementDirection.y, movementDirection.x) * Mathf.Rad2Deg;
-            transform.rotation = Quaternion.Euler(0, 0, angle);
-        }
-
-        lastPosition = transform.position; // Update last position
 
     }
 }
